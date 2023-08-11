b855ddc3a89fb9d3b529a0dd63c03248923f19f6
adair <adair.reyes@spore.cloud>
2023-08-11 13:53:41 -0600

    show popups for each car on map

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,15 +1,50 @@
 <!-- eslint-disable no-undef -->
 <script setup>
 import { useRouter } from 'vue-router'
-import { onMounted } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { getSession } from '../lib/session'
+import icon from '../components/CarIcon/icon.png'
 
+const cars = ref([])
+const map = ref(null)
+const carIcon = L.icon({
+  iconUrl: icon,
+  iconSize: [50, 50]
+})
+
+// Fetch cars
 onMounted(() => {
-  const map = L.map('map').setView([51.505, -0.09], 13)
+  const session = getSession()
+  const authToken = 'Bearer ' + session
+  const url = new URL('http://localhost:3000/cars')
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      authorization: authToken,
+      accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+    .then((res) => res.json())
+    .then((fetchedCars) => (cars.value = fetchedCars))
+    .catch(console.error.bind(console))
+})
+
+watch(cars, (newCars) =>
+  newCars.forEach((car) => {
+    const carOnMap = L.marker([car.position.x, car.position.y], { icon: carIcon })
+    carOnMap.bindPopup(`<b class="popup">${car.plate}<b>`)
+    carOnMap.addTo(map.value)
+  })
+)
+
+// Draw map
+onMounted(() => {
+  map.value = L.map('map').setView([20.710429418405212, -103.40982443626814], 15)
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
-  }).addTo(map)
+  }).addTo(map.value)
 })
 
 const router = useRouter()
@@ -22,7 +57,12 @@ if (!session) {
 
 <template>
   <h1>Home</h1>
-  <div id="map"></div>
+  <div ref="mapNode" id="map"></div>
+  <ul>
+    <li v-for="car in cars" :key="car.id">
+      {{ car.position }}
+    </li>
+  </ul>
 </template>
 
 <style scoped>
@@ -31,5 +71,9 @@ if (!session) {
   aspect-ratio: 4 / 3;
   max-width: 40rem;
   margin-inline: auto;
+}
+
+.popup {
+  color: red;
 }
 </style>
