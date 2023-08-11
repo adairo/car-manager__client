3a4942248962d11355fbc9ab06cfab1ba840b67e
adair <adair.reyes@spore.cloud>
2023-08-11 10:43:08 -0600

    render a leaflet map using the cdn

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,17 @@
+<!-- eslint-disable no-undef -->
 <script setup>
 import { useRouter } from 'vue-router'
+import { onMounted } from 'vue'
 import { getSession } from '../lib/session'
+
+onMounted(() => {
+  const map = L.map('map').setView([51.505, -0.09], 13)
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+  }).addTo(map)
+})
+
 const router = useRouter()
 const session = getSession()
 
@@ -11,4 +22,14 @@ if (!session) {
 
 <template>
   <h1>Home</h1>
+  <div id="map"></div>
 </template>
+
+<style scoped>
+#map {
+  width: 100%;
+  aspect-ratio: 4 / 3;
+  max-width: 40rem;
+  margin-inline: auto;
+}
+</style>
